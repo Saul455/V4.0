efa8ecc038e9c0326a0efa7c469d391fa17a7144
Saul455 <saulr1@hotmail.com>
2024-12-14 21:12:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -40,7 +40,7 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 # if REQUEST_TO_JOIN_MODE is true then force subscribe work like request to join fsub, else if false then work like normal fsub.
 REQUEST_TO_JOIN_MODE = bool(environ.get('REQUEST_TO_JOIN_MODE', False)) # Set True Or False
 TRY_AGAIN_BTN = bool(environ.get('TRY_AGAIN_BTN', False)) # Set True Or False (This try again button is only for request to join fsub not for normal fsub)
-auth_channel = environ.get('AUTH_CHANNEL', -1002443032004') # give your force subscribe channel id here else leave it blank
+auth_channel = environ.get('AUTH_CHANNEL', '-1002443032004') # give your force subscribe channel id here else leave it blank
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002244588420')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
@@ -67,8 +67,8 @@ OWNER_USERNAME = environ.get('OWNER_USERNAME', 'GhostParadis3') # owner username
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+l9jNeG2OCiE0YTQ1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/SigmaDestiny')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/mawhVerify')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/mawhorzzflick')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/mawh_Verify')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+-PbePn-lJFxmMDU1') # Support Chat Link Without https:// or @
 
 # True Or False
